888c6f3a6795e2fdbb34c5d78f8121acbf65208f
Jarrod Medrano <jmedran@gmail.com>
2019-05-13 11:37:29 -0600

    adding stylesheets

--- header.php
+++ header.php
@@ -2,152 +2,174 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
-    <head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119247558-1"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
+<!--[if gt IE 8]><!-->
+<html class="no-js" <?php language_attributes(); ?>>
+<!--<![endif]-->
 
-		  gtag('config', 'UA-119247558-1');
-		</script>
-		
-        
-        <meta name="google-site-verification" content="XC-F291nAypWd2FNWBiTjtZLl9uOYGmZs1DDLgj-9nU" />
-		<meta name="google-site-verification" content="eBDhBbujkN19it6SbjpwI6d5DwEt28P_vtPyyL4VcvU" />
-        <meta charset="<?php bloginfo( 'charset' ); ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119247558-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
 
-        <?php if(get_option('sherpa_use_title') == 'y'): ?>
-        <title><?php wp_title( '|', true, 'right' ); ?></title>
-        <?php endif; ?>
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-        <?php wp_head(); ?>
-        
-        <?php if(!empty(get_option( 'sherpa_schema' ))): ?>
-
-		<?=get_option('sherpa_schema')?>
-            
-        <?php endif; ?>
-		
-		<script type="text/javascript">
-	   if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "jquery.watch.js", "jquery.musemenu.js", "museredirect.js", "index.css"], "outOfDate":[]};
-	</script>
-
-	  <script src="https://www.lawofficeofjaviermartinezjr.com/scripts/museredirect.js?40941066" type="text/javascript"></script>
+    gtag('config', 'UA-119247558-1');
+    </script>
 
 
-	  <link rel="shortcut icon" href="https://www.lawofficeofjaviermartinezjr.com/images/favicon.ico?393290120"/>
-	  <link media="only screen and (max-device-width: 370px)" rel="alternate" href="https://jmjrlawoffice.com/phone/index.html"/>
-	  <!-- CSS -->
-	  <link rel="stylesheet" type="text/css" href="https://www.lawofficeofjaviermartinezjr.com/css/site_global.css?3824112327"/>
-	  <link rel="stylesheet" type="text/css" href="https://www.lawofficeofjaviermartinezjr.com/css/master_contact-master.css?3949542832"/>
-	  <link rel="stylesheet" type="text/css" href="https://www.lawofficeofjaviermartinezjr.com/css/index.css?504952233" id="pagesheet"/>
-	  <!--[if lt IE 9]>
+    <meta name="google-site-verification" content="XC-F291nAypWd2FNWBiTjtZLl9uOYGmZs1DDLgj-9nU" />
+    <meta name="google-site-verification" content="eBDhBbujkN19it6SbjpwI6d5DwEt28P_vtPyyL4VcvU" />
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php if(get_option('sherpa_use_title') == 'y'): ?>
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <?php endif; ?>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+    <?php wp_head(); ?>
+
+    <?php if(!empty(get_option( 'sherpa_schema' ))): ?>
+
+    <?=get_option('sherpa_schema')?>
+
+    <?php endif; ?>
+
+    <script type="text/javascript">
+    if (typeof Muse == "undefined") window.Muse = {};
+    window.Muse.assets = {
+        "required": ["jquery-1.8.3.min.js", "museutils.js", "jquery.watch.js", "jquery.musemenu.js",
+            "museredirect.js", "index.css"
+        ],
+        "outOfDate": []
+    };
+    </script>
+
+    <script src="https://www.lawofficeofjaviermartinezjr.com/scripts/museredirect.js?40941066" type="text/javascript">
+    </script>
+
+
+    <link rel="shortcut icon" href="https://www.lawofficeofjaviermartinezjr.com/images/favicon.ico?393290120" />
+    <link media="only screen and (max-device-width: 370px)" rel="alternate"
+        href="https://jmjrlawoffice.com/phone/index.html" />
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/css/site_global.css?3824112327" />
+    <link rel="stylesheet" type="text/css"
+        href="<?php echo get_stylesheet_directory_uri(); ?>/css/master_contact-master.css?3949542832" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/index.css?504952233"
+        id="pagesheet" />
+    <!--[if lt IE 9]> 
 	  <link rel="stylesheet" type="text/css" href="css/iefonts_index.css?3862442284"/>
 	  <![endif]-->
-	  <!-- Other scripts -->
-	  <script type="text/javascript">
-	   document.documentElement.className += ' js';
-	var __adobewebfontsappname__ = "muse";
-	</script>
-	  <!-- JS includes -->
-	  <script type="text/javascript">
-	   document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//webfonts.creativecloud.com/gudea:n4,n7:all;molengo:n4:all.js" type="text/javascript">\x3C/script>');
-	</script>
-	  <!--[if lt IE 9]>
+    <!-- Other scripts -->
+    <script type="text/javascript">
+    document.documentElement.className += ' js';
+    var __adobewebfontsappname__ = "muse";
+    </script>
+    <!-- JS includes -->
+    <script type="text/javascript">
+    document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//webfonts.creativecloud.com/gudea:n4,n7:all;molengo:n4:all.js" type="text/javascript">\x3C/script>');
+    </script>
+    <!--[if lt IE 9]>
 	  <script src="scripts/html5shiv.js?4241844378" type="text/javascript"></script>
 	  <![endif]-->
-		<!--custom head HTML-->
+    <!--custom head HTML-->
 
-	<!-- Default Statcounter code for Javier Martinez Law
+    <!-- Default Statcounter code for Javier Martinez Law
 https://www.lawofficeofjaviermartinezjr.com/ -->
-<script type="text/javascript">
-var sc_project=11711557; 
-var sc_invisible=1; 
-var sc_security="69495895"; 
-</script>
-<script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js"
-async></script>
-<noscript><div class="statcounter"><a title="Web Analytics
-Made Easy - StatCounter" href="http://statcounter.com/"
-target="_blank"><img class="statcounter"
-src="//c.statcounter.com/11711557/0/69495895/1/" alt="Web
-Analytics Made Easy - StatCounter"></a></div></noscript>
-<!-- End of Statcounter Code -->
+    <script type="text/javascript">
+    var sc_project = 11711557;
+    var sc_invisible = 1;
+    var sc_security = "69495895";
+    </script>
+    <script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+    <noscript>
+        <div class="statcounter"><a title="Web Analytics
+Made Easy - StatCounter" href="http://statcounter.com/" target="_blank"><img class="statcounter"
+                    src="//c.statcounter.com/11711557/0/69495895/1/" alt="Web
+Analytics Made Easy - StatCounter"></a></div>
+    </noscript>
+    <!-- End of Statcounter Code -->
 
 
-	<script type="application/ld+json">
-	{
-	  "@context": {
-		"@vocab": "http://schema.org/"
-	  },
-	  "@graph": [
+    <script type="application/ld+json">
+    {
+        "@context": {
+            "@vocab": "http://schema.org/"
+        },
+        "@graph": [
 
-		{
-		  "@id": "https://www.lawofficeofjaviermartinezjr.com/",
-		  "@type": "Attorney",
-		  "name": "The Law Office of Javier Martinez Jr. P.C.",
-		  "description": "",
-		  "url" : "https://www.lawofficeofjaviermartinezjr.com/",
-		  "image": "",
-		   "sameAs" : [ "https://www.facebook.com/Javier-Martinez-Jr-Law-Office-1476456252647396/",
-		"https://twitter.com/JMartinezJrLaw",]
-		},
+            {
+                "@id": "https://www.lawofficeofjaviermartinezjr.com/",
+                "@type": "Attorney",
+                "name": "The Law Office of Javier Martinez Jr. P.C.",
+                "description": "",
+                "url": "https://www.lawofficeofjaviermartinezjr.com/",
+                "image": "",
+                "sameAs": ["https://www.facebook.com/Javier-Martinez-Jr-Law-Office-1476456252647396/",
+                    "https://twitter.com/JMartinezJrLaw",
+                ]
+            },
 
-		{
-		  "@type": "LocalBusiness",
-		  "parentOrganization": {
-			  "name" : "The Law Office of Javier Martinez Jr. P.C."
-		  },
-		 "name" : "The Law Office of Javier Martinez Jr. P.C.",
-		 "image": "",
-		 "address": { 
-		   "@type": "PostalAddress",
-			   "addressLocality": "El Paso",
-			   "addressRegion": "TX",
-			   "postalCode": "79930",
-			   "streetAddress": "2035 Grant Ave",
-			   "telephone": "(915) 751-0896"
-			  },
-		  "openingHours": ["Mo-Fr 08:00-17:00",],
-		  "hasmap" : "https://www.google.com/maps/place/The+Law+Office+of+Javier+Martinez+Jr.+P.C./@31.7775904,-106.4746794,17z/data=!3m1!4b1!4m5!3m4!1s0x86e759a4fbbe75c1:0x25876aa036af82dd!8m2!3d31.7775904!4d-106.4724907"
-		},
-		{
-		  "@type": "LocalBusiness",
-		  "parentOrganization": {
-			  "name" : "The Law Office of Javier Martinez Jr. P.C."
-		  },
-		 "name" : "The Law Office of Javier Martinez Jr. P.C.",
-		 "image": "",
-		  "address": { 
-			   "@type": "PostalAddress",
-			   "addressLocality": "Hobbs",
-			   "addressRegion": "NM",
-			   "postalCode": "88240",
-			   "streetAddress": "124 E Broadway St",
-			   "telephone": "(575) 393-0021"
-			  },
-		  "openingHours": [ "Mo-Fr 08:00-14:00",],
-		  "hasmap" : "https://www.google.com/maps/place/The+J+Martinez+Law+Firm,+LLC/@32.7011155,-103.1372769,17z/data=!3m1!4b1!4m5!3m4!1s0x86fc950715977569:0x6ef44258e03ee255!8m2!3d32.7011155!4d-103.1350882"
-		}
-	  ]
-	}
-	</script>	
-    </head>
-    <body <?php body_class(); ?>>
-        <div class="clearfix" id="page"><!-- column -->
-		   <div class="position_content" id="page_position_content">
-			<div class="clearfix colelem" id="pu4699"><!-- group -->
-			 <div class="grpelem" id="u4699"><!-- custom html -->
-			  <!--<div itemscope itemtype="">
+            {
+                "@type": "LocalBusiness",
+                "parentOrganization": {
+                    "name": "The Law Office of Javier Martinez Jr. P.C."
+                },
+                "name": "The Law Office of Javier Martinez Jr. P.C.",
+                "image": "",
+                "address": {
+                    "@type": "PostalAddress",
+                    "addressLocality": "El Paso",
+                    "addressRegion": "TX",
+                    "postalCode": "79930",
+                    "streetAddress": "2035 Grant Ave",
+                    "telephone": "(915) 751-0896"
+                },
+                "openingHours": ["Mo-Fr 08:00-17:00", ],
+                "hasmap": "https://www.google.com/maps/place/The+Law+Office+of+Javier+Martinez+Jr.+P.C./@31.7775904,-106.4746794,17z/data=!3m1!4b1!4m5!3m4!1s0x86e759a4fbbe75c1:0x25876aa036af82dd!8m2!3d31.7775904!4d-106.4724907"
+            },
+            {
+                "@type": "LocalBusiness",
+                "parentOrganization": {
+                    "name": "The Law Office of Javier Martinez Jr. P.C."
+                },
+                "name": "The Law Office of Javier Martinez Jr. P.C.",
+                "image": "",
+                "address": {
+                    "@type": "PostalAddress",
+                    "addressLocality": "Hobbs",
+                    "addressRegion": "NM",
+                    "postalCode": "88240",
+                    "streetAddress": "124 E Broadway St",
+                    "telephone": "(575) 393-0021"
+                },
+                "openingHours": ["Mo-Fr 08:00-14:00", ],
+                "hasmap": "https://www.google.com/maps/place/The+J+Martinez+Law+Firm,+LLC/@32.7011155,-103.1372769,17z/data=!3m1!4b1!4m5!3m4!1s0x86fc950715977569:0x6ef44258e03ee255!8m2!3d32.7011155!4d-103.1350882"
+            }
+        ]
+    }
+    </script>
+</head>
+
+<body <?php body_class(); ?>>
+    <div class="clearfix" id="page">
+        <!-- column -->
+        <div class="position_content" id="page_position_content">
+            <div class="clearfix colelem" id="pu4699">
+                <!-- group -->
+                <div class="grpelem" id="u4699">
+                    <!-- custom html -->
+                    <!--<div itemscope itemtype="">
 		   <span itemprop="name">Law Office of Javier Martinez Jr P.C</span>
 		   <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
 			 <span itemprop="streetAddress">2035 GRANT AVE.</span>
@@ -158,39 +180,59 @@ Analytics Made Easy - StatCounter"></a></div></noscript>
 		   Phone: <span itemprop="telephone">915-751-0896</span>
 		</div>-->
 
-		</div>
-			 <div class="shadow grpelem" id="u2056"><!-- simple frame --></div>
-			 <div class="browser_width grpelem" id="u2057-bw">
-			  <div class="shadow" id="u2057"><!-- group -->
-			   <div class="clearfix" id="u2057_align_to_page">
-				<div class="clip_frame grpelem" id="u4384"><!-- image -->
-				 <img class="block" id="u4384_img" src="https://www.lawofficeofjaviermartinezjr.com/images/thinkstockphotos-480419149.png" alt="" width="80" height="80"/>
-				</div>
-				<div class="clearfix grpelem" id="u2060-4"><!-- content -->
-				 <p>Law Office of Javier Martinez, Jr., P.C</p>
-				</div>
-				<div class="clearfix grpelem" id="u2064-4"><!-- content -->
-				 <p>(915) 751-0896 • (575) 393-0021</p>
-				 <p>(505) 563-5888 • (505) 395-4450</p>
-				</div>
-				<div class="shadow grpelem" id="u2065"><!-- simple frame --></div>
-				<a class="nonblock nontext clearfix grpelem" id="u2063-4" href="/contact" data-href="page:U368"><!-- content --><p>Contact</p></a>
-				<div class="shadow grpelem" id="u2068"><!-- simple frame --></div>
-				<a class="nonblock nontext clip_frame grpelem" id="u2069" href="https://www.facebook.com/pages/Javier-Martinez-Jr-Law-Office/1476456252647396"><!-- image --><img class="block" id="u2069_img" src="https://www.lawofficeofjaviermartinezjr.com/images/facebook.png" alt="Facebook" title="Facebook" width="10" height="22"/></a>
-			   </div>
-			  </div>
-			 </div>
-			</div>
-        <section id="header">
-	        <div class="container">
-		        <div class="row">
-			        <div class="<?=FULLWIDTH?>">
-				        <?=__('Header','sherpa')?>
-			        </div>
-		        </div>
-		        <div class="row">
-			        <div class="<?=FULLWIDTH?>">
-				        <?php
+                </div>
+                <div class="shadow grpelem" id="u2056">
+                    <!-- simple frame -->
+                </div>
+                <div class="browser_width grpelem" id="u2057-bw">
+                    <div class="shadow" id="u2057">
+                        <!-- group -->
+                        <div class="clearfix" id="u2057_align_to_page">
+                            <div class="clip_frame grpelem" id="u4384">
+                                <!-- image -->
+                                <img class="block" id="u4384_img"
+                                    src="https://www.lawofficeofjaviermartinezjr.com/images/thinkstockphotos-480419149.png"
+                                    alt="" width="80" height="80" />
+                            </div>
+                            <div class="clearfix grpelem" id="u2060-4">
+                                <!-- content -->
+                                <p>Law Office of Javier Martinez, Jr., P.C</p>
+                            </div>
+                            <div class="clearfix grpelem" id="u2064-4">
+                                <!-- content -->
+                                <p>(915) 751-0896 • (575) 393-0021</p>
+                                <p>(505) 563-5888 • (505) 395-4450</p>
+                            </div>
+                            <div class="shadow grpelem" id="u2065">
+                                <!-- simple frame -->
+                            </div>
+                            <a class="nonblock nontext clearfix grpelem" id="u2063-4" href="/contact"
+                                data-href="page:U368">
+                                <!-- content -->
+                                <p>Contact</p>
+                            </a>
+                            <div class="shadow grpelem" id="u2068">
+                                <!-- simple frame -->
+                            </div>
+                            <a class="nonblock nontext clip_frame grpelem" id="u2069"
+                                href="https://www.facebook.com/pages/Javier-Martinez-Jr-Law-Office/1476456252647396">
+                                <!-- image --><img class="block" id="u2069_img"
+                                    src="https://www.lawofficeofjaviermartinezjr.com/images/facebook.png" alt="Facebook"
+                                    title="Facebook" width="10" height="22" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section id="header">
+                <div class="container">
+                    <div class="row">
+                        <div class="<?=FULLWIDTH?>">
+                            <?=__('Header','sherpa')?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="<?=FULLWIDTH?>">
+                            <?php
 							// Call either the responsive_bs_menu() or the non_responsive_bs_menu() here
 							//  If you are using a menu besides 'primary', use this as the first argument in the function.
 							//  You can control whether the menu is right- or left-aligned with $menu_side.  It will be left-aligned unless you set
@@ -204,8 +246,7 @@ Analytics Made Easy - StatCounter"></a></div></noscript>
 
                             echo responsive_bs_menu('primary');
 				        ?>
-			        </div>
-		        </div>
-	        </div>
-        </section>
-		
+                        </div>
+                    </div>
+                </div>
+            </section>
